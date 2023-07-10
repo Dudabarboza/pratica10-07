6c07415fed99364ff5b96bb24cddc2d543074337
Maria Eduarda Barboza <108808310+Dudabarboza@users.noreply.github.com>
2023-07-10 12:33:21 -0300

    Add files via upload

---
+++ pratica-back/index.js
@@ -0,0 +1,45 @@
+//-------Atividade 01--------//
+// const http = require('http')
+// const PORT = 5000
+
+// const server = http.createServer((req,res)=> {
+//     res.setHeader{200, ('Content-Type', 'application/json')};
+// })
+// server.listen(5000)
+
+// -------Atvidade 02--------//
+
+// const path = require('path')
+// path.parse('https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf')
+
+//-------Atividade 03--------//
+// const url = require('url');
+
+// url.format({
+//     protocol: 'https',
+//     slaches: true,
+//     auth: null,
+//     host: 'nodejs.org',
+//     hostname: 'nodejs.org',
+//     hash: '#url_nem_input_base',
+//     search: null,
+//     query: [Object: null prototype] {},
+//     pathname: '/api/url.html',
+//     path:'/api/url.html',
+//     href: 'https://node.js.org/api/url.html#url_new_url_input_base'
+// })
+
+//------Atividade 04-------//
+
+// const fs = require('fs')
+
+
+
+
+
+// //------Atividade 05-------//
+
+// const os = require('os')
+// os.cpus(
+
+// )
